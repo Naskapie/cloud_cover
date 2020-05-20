71cd940dcb86085adf4c10dcd075086cd40f7482
Naskapie <naskapie@gmx.net>
2020-05-20 18:55:32 +0200

    Add Weather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -21,18 +22,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
         : location.latitude.toString() + ', ' + location.longitude.toString());
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    print(response.statusCode);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location: '),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
