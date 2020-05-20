a3973887bd340871f170a34b8b208b7d4b03fe05
Naskapie <naskapie@gmx.net>
2020-05-20 17:26:12 +0200

    Add Location

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,8 +1,18 @@
-import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-class LoadingScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+class Location {
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
   }
 }
